33ee0e4c3269e51027b6473118ef19a6d4cc90b3
ShibanshL <shibansh.w@gmail.com>
2022-04-25 15:52:57 +0530

    Updae number 32

--- NewFile.js
+++ NewFile.js
@@ -3,16 +3,20 @@ const emObj = new emmiter()  //object
 
 var link = 'https://myinfo.io/'
 
-console.log(__filename)
-console.log(__dirname)
-
-function log(message){
-    console.log(message)
-    // console.log(module)
-
-    emObj.emit('messageLogged',{id:1, url:'http://'})
-
+// console.log(__filename)
+// console.log(__dirname)
+class Logger{
+    log(message){
+        console.log(message)
+        // console.log(module)
+    
+        emObj.emit('messageLogged',{id:1, url:'http://'})
+    
+    }
 }
+
+
+
 
 
 
