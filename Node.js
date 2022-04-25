d446e6bed998c76b3a1c7b927831abc1a5543516
ShibanshL <shibansh.w@gmail.com>
2022-04-25 14:25:37 +0530

    Another predefined Module

--- Node.js
+++ Node.js
@@ -1,6 +1,18 @@
-const path = require('path')
+// const path = require('path')
 
+const os = require('os')
 
-var pathObj = path.parse(__filename)
+// var pathObj = path.parse(__filename)
 
-console.log(pathObj)
+var osObj = os.freemem()
+var osObj1 = os.totalmem()
+var osObj2 = os.platform
+var osObj3 = os.uptime
+
+console.log('Obj = ',osObj)
+console.log('Obj1 = ',osObj1)
+console.log('Obj2 = ',osObj2)
+console.log('Obj3 = ',osObj3)
+// console.log('Obj = ',osObj)
+
+// console.log(pathObj)
