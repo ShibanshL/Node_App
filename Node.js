69f58090913691a7b11e99734b54314b34368345
ShibanshL <shibansh.w@gmail.com>
2022-04-25 14:46:58 +0530

    Another module (File System)

--- Node.js
+++ Node.js
@@ -2,17 +2,21 @@
 
 const os = require('os')
 
+const fs = require('fs')
+
 // var pathObj = path.parse(__filename)
 
 var osObj = os.freemem()
 var osObj1 = os.totalmem()
 var osObj2 = os.platform()
 var osObj3 = os.uptime()
+var file = fs.readFileSync('./')
 
-console.log('Obj = ',osObj)
-console.log('Obj1 = ',osObj1)
-console.log('Obj2 = ',osObj2)
-console.log('Obj3 = ',osObj3)
+// console.log('Obj = ',osObj)
+// console.log('Obj1 = ',osObj1)
+// console.log('Obj2 = ',osObj2)
+// console.log('Obj3 = ',osObj3)
+console.log(file)
 // console.log('Obj = ',osObj)
 
 // console.log(pathObj)
